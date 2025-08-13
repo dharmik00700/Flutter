ea65e9437a4e4a9361236e01598c1fac86cf5999
dharmik00700 <132027641+dharmik00700@users.noreply.github.com>
2025-08-13 16:31:20 +0530

    task 1
    Write a program that takes a user's name and age as input and prints a welcome message that
     includes their name and how many years they have left until they turn 100.

--- Assignments/module-2/1_module-2.dart
+++ Assignments/module-2/1_module-2.dart
@@ -13,3 +13,9 @@ void main() {
   print(
       "Welcome, $username your age is $ageuser and your out of 100 years left year is $leftyear");
 }
+/*Enter Your name :-
+Chansik
+Enter Your age :-
+19
+Welcome, Chansik your age is 19 and your out of 100 years left year is 81
+*/
