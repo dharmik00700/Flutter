aeda1e15f6e45fec52cbfa59b16d523a2a0b7e5a
dharmik00700 <132027641+dharmik00700@users.noreply.github.com>
2025-08-14 20:17:24 +0530

    task 17
     Create a class called Book with properties like title, author, and publication year. Add
     methods to display the book’s details and a method to check if it’s over 10 years old.

---
+++ Assignments/module-2/17_module-2.dart
@@ -0,0 +1,35 @@
+class Book {
+  String title;
+  String author;
+  int publicationYear;
+
+  Book(this.title, this.author, this.publicationYear);
+
+  void displayDetails() {
+    print('Title: $title');
+    print(' Author: $author');
+    print('Published: $publicationYear');
+  }
+
+  bool isOver10YearsOld() {
+    int currentYear = DateTime.now().year;
+    return currentYear - publicationYear > 10;
+  }
+}
+
+void main() {
+  Book myBook = Book('The Alchemist', 'Paulo Coelho', 2005);
+  myBook.displayDetails();
+
+  if (myBook.isOver10YearsOld()) {
+    print('This book is over 10 years old.');
+  } else {
+    print('This book is less than 10 years old.');
+  }
+}
+/*
+Title: The Alchemist
+ Author: Paulo Coelho
+Published: 2005
+This book is over 10 years old.
+ */
