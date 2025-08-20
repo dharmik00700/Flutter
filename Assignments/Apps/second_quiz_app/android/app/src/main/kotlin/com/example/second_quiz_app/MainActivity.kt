f682cd85d0f6da82d044253252871b7b9748d620
dharmik00700 <132027641+dharmik00700@users.noreply.github.com>
2025-08-20 12:41:03 +0530

    same error code reason

--- Assignments/Apps/second_quiz_app/android/app/src/main/kotlin/com/example/second_quiz_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.second_quiz_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Assignments/Apps/second_quiz_app/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:second_quiz_app/screens/quiz.dart';
-// import 'package:second_quiz_app/screens/start_screen.dart';
-
-void main() {
-  runApp(const quiz_screen());
-}
-
-
